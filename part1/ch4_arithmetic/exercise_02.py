0c76dba7ca85330c9268059a83a60d1f8518773a
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:09:49 +0200

    Create exercise_02.py

---
+++ part1/ch4_arithmetic/exercise_02.py
@@ -0,0 +1,7 @@
+# Exercise: 2
+# Task: Ask for name and birth year, print age at end of 2021
+
+name = input("What is your name? ")
+year = int(input("Which year were you born? "))
+
+print(f"Hi {name}, you will be {2021 - year} years old at the end of the year 2021")
