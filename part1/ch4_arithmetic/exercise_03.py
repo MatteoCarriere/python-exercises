decc84d9a98b7752a2ed0f1cb112242dd2d51fa7
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:10:13 +0200

    Create exercise_03.py

---
+++ part1/ch4_arithmetic/exercise_03.py
@@ -0,0 +1,5 @@
+# Exercise: 3
+# Task: Ask for a number of days and print the equivalent number of seconds
+
+days = int(input("How many days? "))
+print(f"Seconds in that many days: {days * 24 * 60 * 60}")
