27d51883686b6c64f1c3b919e127d0276e4cf5be
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 17:59:10 +0200

    Create exercise_02.py

---
+++ part1/ch1_intro/exercise_02.py
@@ -0,0 +1,10 @@
+# Exercise: 2
+# Task: Print the names of the seven brothers in alphabetical order
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
