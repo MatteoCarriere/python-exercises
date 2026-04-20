4edb3a917d2c7b244322ed6062353142ecd83d61
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:03:08 +0200

    Create exercise_03.py

---
+++ part1/ch2_variables/exercise_03.py
@@ -0,0 +1,12 @@
+# Exercise: 3
+# Task: Ask for given name, family name, street address, city and postal code,
+#       then print them formatted as a mailing label
+
+given_name = input("Given name: ")
+family_name = input("Family name: ")
+street_address = input("Street address: ")
+city_and_postal_code = input("City and postal code: ")
+
+print(given_name + " " + family_name)
+print(street_address)
+print(city_and_postal_code)
