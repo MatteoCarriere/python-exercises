6da10f50d13ca8e5d65e8cca6ed774afb879f150
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:10:57 +0200

    Create exercise_05.py

---
+++ part1/ch4_arithmetic/exercise_05.py
@@ -0,0 +1,8 @@
+# Exercise: 5
+# Task: Ask for two numbers and print their sum and product
+
+n1 = int(input("Number 1: "))
+n2 = int(input("Number 2: "))
+
+print(f"The sum of the numbers: {n1 + n2}")
+print(f"The product of the numbers: {n1 * n2}")
