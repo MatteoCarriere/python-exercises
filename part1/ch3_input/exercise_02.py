8f1cdd663eb8f1e0affd1707733753a78625f38e
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:08:31 +0200

    Create exercise_02.py

---
+++ part1/ch3_input/exercise_02.py
@@ -0,0 +1,11 @@
+# Exercise: 2
+# Task: Given x and y, print addition, subtraction, multiplication and division
+#       — must work correctly for any values of x and y
+
+x = 27
+y = 15
+
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} * {y} = {x * y}")
+print(f"{x} / {y} = {x / y}")
