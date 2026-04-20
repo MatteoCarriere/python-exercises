01d443ca372088e81376d53e8fbd4a761917a316
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 17:54:37 +0200

    Create exercise_01.py

---
+++ part1/ch1_intro/exercise_01.py
@@ -0,0 +1,19 @@
+# This program is supposed to print out the names of the brothers in alphabetical order
+
+# Fix the code
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")
+
+# Code fixed
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
