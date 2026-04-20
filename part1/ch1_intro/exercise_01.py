98c8706f61b11ff1e33fb049f0edc982b8cfbe0b
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 17:58:36 +0200

    Update exercise_01.py

--- part1/ch1_intro/exercise_01.py
+++ part1/ch1_intro/exercise_01.py
@@ -1,19 +1,4 @@
-# This program is supposed to print out the names of the brothers in alphabetical order
+# Exercise: 1
+# Task: Print an emoticon :-)
 
-# Fix the code
-print("Simeoni")
-print("Juhani")
-print("Eero")
-print("Lauri")
-print("Aapo")
-print("Tuomas")
-print("Timo")
-
-# Code fixed
-print("Aapo")
-print("Eero")
-print("Juhani")
-print("Lauri")
-print("Simeoni")
-print("Timo")
-print("Tuomas")
+print(":-)")
