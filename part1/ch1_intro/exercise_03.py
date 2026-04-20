bc12f5d825b773c71f317310437e3828e0756fbd
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 17:59:39 +0200

    Create exercise_03.py

---
+++ part1/ch1_intro/exercise_03.py
@@ -0,0 +1,7 @@
+# Exercise: 3
+# Task: Print the lyrics of "Row, Row, Row Your Boat" exactly as given
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
