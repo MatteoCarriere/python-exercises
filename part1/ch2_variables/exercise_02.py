b25b74d99fc2c3b7647f9ab055a3a2cea73b899f
Matteo Carriere <99682132+MatteoCarriere@users.noreply.github.com>
2026-04-20 18:02:46 +0200

    Create exercise_02.py

---
+++ part1/ch2_variables/exercise_02.py
@@ -0,0 +1,6 @@
+# Exercise: 2
+# Task: Ask for the user's name and print it twice on one line with
+#       exclamation marks at the start, between the two names, and at the end
+
+name = input("What is your name? ")
+print("!" + name + "!" + name + "!")
