9e5f8f2248e732810860ba710abeafaa401aa933
MatteoCarriere <carrierematteo75@gmail.com>
2026-04-22 20:47:47 +0200

    add: exercise_03.py

---
+++ part1/ch5_conditionals/exercise_03.py
@@ -0,0 +1,11 @@
+# Exercise: 3
+# Task: If the name is anything but "Jerry", it prints out the total cost
+
+name = (input("Please tell me your name: "))
+
+if name != 'Jerry':
+    num_soup =  int(input("How many portions of soup? "))
+    print(f"The total cost is {num_soup * 5.90}")
+    print(f"Next please!")
+
+print(f"Next please!")
