d2eedff66cf41846808220e6a5bd0c083146771a
MatteoCarriere <carrierematteo75@gmail.com>
2026-06-12 20:58:24 +0200

    Add even numbers exercise

---
+++ part3/ch1_loops_with_conditions/exercise_01.py
@@ -0,0 +1,8 @@
+# Programming exercise: Even numbers
+# Prints all even numbers between 2 and 30 (inclusive), one per line.
+
+number = 2
+
+while number <= 30:
+    print(f"{number}")
+    number += 2
